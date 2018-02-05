6981740da1ed3683f8e235156b46f18959881f38
iris-mcdonald <32845453+iris-mcdonald@users.noreply.github.com>
2018-02-05 11:49:35 -0500

    Add files via upload

---
+++ Script1.js
@@ -0,0 +1,147 @@
+    function setBoxId(box) 
+    {
+        var result = ' ';
+        if (box == 1) {
+          result = "a";
+        } else if (box == 2) {
+          result = "b";
+        } else {
+          result = "c";
+        }
+        return result;
+    }//end of setBoxId
+
+        function chgBoxColor(boxId)
+        {
+            var result = 0;
+            while ((result == 0) || (result > 3)) {//run for each box
+                var result = myRandomNumber();
+            }
+
+            /*assigns class name-in css class assigns background color*/
+            /*debugger;*/
+            /*alert("result = " + result);*/
+            
+            
+            switch (result)
+            {
+                case 1:
+                    /*document.write('Case One: ' + result);*/
+                    document.getElementById(boxId).className = "one";
+                    break;
+                case 2:
+                    /*document.write('Case Two: ' + result);*/
+                    document.getElementById(boxId).className = "two";
+                    break;
+                case 3:
+                    /*document.write('Case Three: ' + result);*/
+                    document.getElementById(boxId).className = "three";
+                    /*$('"#" + boxId').attr('class' , 'three');*/
+                    break;
+                default:
+                    document.write('switch case Not One, Two, or Three ' + result);//class not changed
+                    break;
+            } 
+
+            return result;
+        }
+
+        function myRandomNumber()
+        {
+            var result = Math.floor((Math.random() * 10) + 1);
+            /*document.write("random generator: " + result);*/
+            return result;
+        }
+
+        function setWinOrLoseMsg(array_BoxColors)
+        {
+            /*document.write(" SetWinOrLoseMsg array(1)= " + array_BoxColors[1] + " " +
+                array_BoxColors[2] + " " + array_BoxColors[3]);*/
+            if ((array_BoxColors[1] == array_BoxColors[2]) &&
+                (array_BoxColors [2] == array_BoxColors[3])) {
+                 /*document.getElementById("msg").innerHTML = "Congratulations, you won!";*/
+                $("#msg").text("Congratulations, you won!");
+            } else {
+                 /*document.getElementById("msg").innerHTML = "Sorry, try again!";*/
+                $("#msg").text("Sorry, try again!");
+            }
+        }//end of setWinOrLoseMsg
+
+        function startProcess()
+        {
+            
+            $("#btn").hover(function(){
+                alert("You entered hover!");
+                $("#btn").text("SPIN NOW");
+                $("#btn").css(
+                    {
+                    'background-color': 'darkgreen'
+                    })
+            });
+            /*function(){
+            alert("Bye! You now leave p1!");
+            });*/
+
+            /*debugger;*/
+                
+            /*$('#btn').on('hover', function()
+            {
+                alert('hovering caught');
+            });*/
+            /*{
+                $('#btn').css(
+                    {'background-color': 'darkgreen',
+                     'font-size': '20px'
+                     });
+                $('#btn').text('SPIN NOW');
+            });*/
+
+            var array_BoxColors = [0,0,0,0];//declare array for storing box color results
+
+            for (var box = 1; box <= 3; box++)//process the three text boxes
+            {
+                var boxId = setBoxId(box);//set id value for the text box that you're processsing
+                               
+                for ( var i = 1; i <= 10; i++)//run change box color function 10X 
+                {
+                    var colorOfBox = chgBoxColor(boxId);//returns 1,2,or 3 representing which color set
+                    array_BoxColors[box] = colorOfBox;                             
+                } 
+            }//end of process text box loop
+            /*document.write("*** array values:  " + array_BoxColors[1] + " " + array_BoxColors[2]
+                + " " + array_BoxColors[3] + " *** ");*/
+
+            setWinOrLoseMsg(array_BoxColors);//display win or lose msg based on text box colors 
+            
+                            
+        }//end of startProcess
+
+    
+       
+    
+           
+$(document).ready(function()
+{
+    console.log( "ready!" );
+    startProcess();
+}    
+);
+    
+   
+    
+
+
+
+
+
+
+
+   
+   
+
+
+        
+
+     
+
+   
